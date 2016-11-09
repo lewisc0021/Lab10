7f4bd1e0e4163f49ad2612bc32e0653c0f6a064d
Tommy Lewis <lewisc0021@gmail.com>
2016-11-08 21:13:14 -0500

    using MovieIO class, refractoring into logical functions, useful comments

--- Lab10_Extended/Program.cs
+++ Lab10_Extended/Program.cs
@@ -11,141 +11,167 @@ namespace Lab10_Extended
     {
         static void Main(string[] args)
         {
-            //Setting up the Hash Table/////////////////////////////////////////////////////////////////
+            // 1.
+            Display();
+
+            // 2.
+            Dictionary<string, List<string>> Movies = CreateDictionary(); 
+
+            while (true)// Primary Functional Loop Start
+            {
+                //3.
+                int selection = GetSelection();
+
+                //4.
+                string answer = GetAnswer(selection);
+
+                //5.
+                List<string> finalList = CategorySearch(answer, Movies);
+                foreach (string item in finalList)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+                //Continue?
+                Console.WriteLine("Enter anything to continue, or no to exit");
+                string cont = Console.ReadLine().Trim().ToLower();
+                if (cont == "n" || cont == "no")
+                    break;
+                else 
+                    Console.WriteLine("Please enter another number");
+
+            }// Primary Functional Loop End
+
+
+        }//MAIN---------------------
+
+        // 1. Displaying the menu (an all string method)
+        private static void Display()
+        {
+            Console.WriteLine("                  Welcome to the Movie Directory          ");
+            Console.WriteLine("==========================================================");
+            Console.WriteLine("                           MENU                           ");
+            Console.WriteLine("==========================================================");
+            Console.WriteLine("Scifi || Drama || Animated || Horror || Musical || Comedy ");
+            Console.WriteLine();
+            Console.WriteLine("   1  ||   2   ||    3     ||    4   ||    5    ||    6   ");
+            Console.WriteLine();
+            Console.WriteLine("Directions");
+            Console.WriteLine("Enter the number cooresponding to your desired category");
+            Console.WriteLine("=========");
+            Console.WriteLine("Please enter a number (1-6) for a movie category.");
+        }
+
+        // 2. Returns the Dictionary that provides central functionality to the program
+        private static Dictionary<string, List<string>> CreateDictionary()
+        {
+            //STEP 1: Initialize Hash Table (Dictionary), Category Lists and Master List
             Dictionary<string, List<string>> Movies = new Dictionary<string, List<string>>();
             List<string> ScifiMovies = new List<string>();
             List<string> DramaMovies = new List<string>();
             List<string> AnimatMovies = new List<string>();
             List<string> HorrorMovies = new List<string>();
+            List<string> Musicals = new List<string>();
+            List<string> Comedies = new List<string>();
             List<Movie> AllMovies = new List<Movie>();
-            ////////////////////////////////////////////////////////////////////////////////////////////
-            AllMovies.Add(new Movie("Star Wars", "science fiction"));
-            AllMovies.Add(new Movie("A Space Odyssey", "science fiction"));
-            AllMovies.Add(new Movie("A Clockwork Orange", "science fiction"));
-            AllMovies.Add(new Movie("Inception", "science fiction"));
-            AllMovies.Add(new Movie("Saving Private Ryan", "drama"));
-            AllMovies.Add(new Movie("The Prestige", "drama"));
-            AllMovies.Add(new Movie("Friday Night Lights", "drama"));
-            AllMovies.Add(new Movie("Forest Gump", "drama"));
-            AllMovies.Add(new Movie("Finding Nemo", "animated"));
-            AllMovies.Add(new Movie("Mulan", "animated"));
-            AllMovies.Add(new Movie("Monsters Inc.", "animated"));
-            AllMovies.Add(new Movie("The Incredibles", "animated"));
-            AllMovies.Add(new Movie("The Witch", "horror"));
-            AllMovies.Add(new Movie("IT", "horror"));
-            AllMovies.Add(new Movie("The Ring", "horror"));
-            AllMovies.Add(new Movie("", "horror"));
-            ////////////////////////////////////////////////////////////////////////////////////////////
+
+            //STEP 2: Populate the Master List from MovieIO items
+            for (int i = 1; i <= 100; i++)
+            {
+                AllMovies.Add(MovieIO.getMovie(i));
+            }
+
+            //STEP 3: Populate each Category List
             foreach (Movie item in AllMovies)
             {
-                if (item.CATEGORY == "science fiction")
+                if (item.CATEGORY == "scifi")
                     ScifiMovies.Add(item.TITLE);
+
                 if (item.CATEGORY == "drama")
                     DramaMovies.Add(item.TITLE);
+
                 if (item.CATEGORY == "animated")
                     AnimatMovies.Add(item.TITLE);
+
                 if (item.CATEGORY == "horror")
                     HorrorMovies.Add(item.TITLE);
+
+                if (item.CATEGORY == "musical")
+                    Musicals.Add(item.TITLE);
+
+                if (item.CATEGORY == "comedy")
+                    Comedies.Add(item.TITLE);
             }
-            ////////////////////////////////////////////////////////////////////////////////////////////
-            Movies["science fiction"] = ScifiMovies;
+
+            //STEP 4: Match Dictionary keys to category lists
+            Movies["scifi"] = ScifiMovies;
             Movies["drama"] = DramaMovies;
             Movies["animated"] = AnimatMovies;
             Movies["horror"] = HorrorMovies;
-            ////////////////////////////////////////////////////////////////////////////////////////////
-            ////////////////////////////////////////////////////////////////////////////////////////////
-            //Display and functionality
-            Console.WriteLine("       Welcome to the Movie Directory       ");
-            Console.WriteLine("============================================");
-            Console.WriteLine("                    MENU                    ");
-            Console.WriteLine("============================================");
-            Console.WriteLine("Drama  | Science Fiction | Animated | Horror");
-            Console.WriteLine();
-            Console.WriteLine("   1   |        2        |    3     |    4  ");
-            Console.WriteLine();
-            Console.WriteLine("Directions");
-            //================================
-            Console.WriteLine("=========");
-            Console.WriteLine("Please enter a number (1-4) for a movie category.");
-            while (true)
-            {
+            Movies["musical"] = Musicals;
+            Movies["comedy"] = Comedies;
+
+            return Movies;
+        }
+
+        // 3. Returns selection number
+        private static int GetSelection()
+        {
             int selection;
             while (true)
             {
                 while (!int.TryParse(Console.ReadLine(), out selection))
-                    {
-                        Console.WriteLine("Please input a correct value.");
-                    }
-                    if (selection >= 1 && selection <= 4)
-                        break;
-                    else
-                        Console.WriteLine("Value needs to be from 1 to 4.");
-                        Console.WriteLine("Please enter another number (1-4) for a movie category.");
-                }
-                ///////////////////////////////////////////////////////////////////////////////////////////////////
-                ///////////////////////////////////////////////////////////////////////////////////////////////////
-                string answer="";
-                switch (selection)
                 {
-                    case 1:
-                        answer = "drama";
-                        break;
-                    case 2:
-                        answer = "science fiction";
-                        break;
-                    case 3:
-                        answer = "animated";
-                        break;
-                    case 4:
-                        answer = "horror";
-                        break;
+                    Console.WriteLine("Please input a correct value.");
                 }
-                ///////////////////////////////////////////////////////////////////////////////////////////////////
-                ///////////////////////////////////////////////////////////////////////////////////////////////////
-                    List<string> finalList = CategorySearch(answer, Movies);
-                    foreach (string item in finalList)
-                    {
-                        Console.WriteLine(item);
-                    }
-                    Console.WriteLine("Do you wish to continue? (y/n)");
-                    string cont = Console.ReadLine();
-                    if (cont == "n" || answer == "N")
-                        break;
-                Console.WriteLine("Please enter another number");
+                if (selection >= 1 && selection <= 6)
+                    break;
+                else
+                    Console.WriteLine("Value needs to be from 1 to 6.");
+                Console.WriteLine("Please enter another number (1-6) for a movie category.");
             }
-        }//MAIN---------------------
+            return selection;
+        }
 
-        //Returns cooresponding hashtable (dictionary) items for given key
+        // 4. Returns answer that is used for category identification
+        private static string GetAnswer(int selection)
+        {
+            string answer = "";
+            switch (selection)
+            {
+                case 1:
+                   return answer = "scifi";
+                 
+                case 2:
+                   return answer = "drama";
+                   
+                case 3:
+                   return answer = "animated";
+             
+                case 4:
+                    return answer = "horror";
+                 
+                case 5:
+                   return answer = "musical";
+                   
+                case 6:
+                   return answer = "comedy";
+
+                default:
+                    return answer = "nothing";
+            }
+        }
+
+        // 5. Returns cooresponding hashtable (dictionary) items for given key
         public static List<string> CategorySearch(string answer, Dictionary<string, List<string>> Movies)
         {
             List<string> finalList = new List<string>();
-            List<string> SortedfinalList = new List<string>();
             finalList = Movies[answer];
             finalList.Sort();
+
             return finalList;
         }
-              
-        //Validation of input
-        public static bool validateAnswer(string answer)
-        {
-            List<string> possibleAnsrs = new List<string>();
-            possibleAnsrs.Add("science fiction");
-            possibleAnsrs.Add("drama");
-            possibleAnsrs.Add("animated");
-            possibleAnsrs.Add("horror");
-            answer = answer.Trim();
-            if (answer == "")
-            {
-                Console.WriteLine("Please actually enter an input.");
-                return false; //triggered if the user didn't enter any characters ie. spaces or tabs or nothing
-            }
-            if (!possibleAnsrs.Contains(answer))
-            {
-                Console.WriteLine("Category not found.");
-                return false; //triggered if the user didn't enter anything that matched
-            }
-            else
-                return true;
-        }
-
+             
+         
 }}//END----------------------
